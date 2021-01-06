05d1572af5bc408edb407bb24f0b3158b87c64ee
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-01-06 19:59:17 +0200

    updated

--- js/task-8.js
+++ js/task-8.js
@@ -8,3 +8,19 @@
 // Размеры самого первого div - 30px на 30px
 // Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
+
+const inputRef = document.querySelector('#controls > input');
+const renderBtnRef = document.querySelector('button[data-action="render"]');
+const destroyBtnRef = document.querySelector('button[data-action="destroy"]');
+const collectionBlockRef = document.querySelector('#boxes');
+
+const createBoxes = amount => {};
+
+const createRandomColor = () => {
+  const r = Math.round(Math.random() * (256.5 - 0.5) + 0.5) - 1;
+  const g = Math.round(Math.random() * (256.5 - 0.5) + 0.5) - 1;
+  const b = Math.round(Math.random() * (256.5 - 0.5) + 0.5) - 1;
+  return `${(r, g, b)}`;
+};
+
+inputRef.addEventListener('input', event => {});
