57c88769e7b8bc8418ad0159b7079bbe081fdce8
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-01-11 15:43:30 +0200

    Update task-8.js

--- js/task-8.js
+++ js/task-8.js
@@ -39,7 +39,6 @@ function createRandomColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  console.log(`rgb(${r}, ${g}, ${b})`);
   return `rgb(${r}, ${g}, ${b})`;
 }
 
